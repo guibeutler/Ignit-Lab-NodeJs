b5f5eb03a64443e9ef68f4b60c7ff82ec0ce7806
Guilherme <guilherme.beutler@outlook.com>
2022-12-14 15:50:12 -0300

    adjustments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './infra/app.controller';
-import { PrismaService } from './prisma.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './infra/app.controller'
+import { PrismaService } from './prisma.service'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [PrismaService],
+	imports: [],
+	controllers: [AppController],
+	providers: [PrismaService],
 })
 export class AppModule {}
